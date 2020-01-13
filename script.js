d0466278548f3e973f03b48a000c78d03d25b55e
Rafael Hautekiet <rafaelhautekiet@gmail.com>
2020-01-13 16:08:05 +0100

    update code with dutch comments

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const sneakerWrapper = document.querySelector(".sneaker-wrapper")
+
+// selecteer onze kleur paden en stop ze in variabele
 const colorAccentTop = document.querySelectorAll(".accent_top")
 const colorAccentBottom = document.querySelectorAll(".accent_bottom")
 const colorBaseFront = document.querySelectorAll(".base_front")
 const colorBaseBack = document.querySelectorAll(".base_back")
-const stars = document.querySelectorAll(".star")
-const sneakers = document.getElementById("sneakers")
-const data_size = document.getElementById("data_size")
-const flush = document.getElementById("flush")
 
 window.localStorage.trainingData = window.localStorage.trainingData || JSON.stringify([])
 
-// our current voting combination
+// huidige kleuren
+
 const currentColors = {
   accent_top: {},
   accent_bottom: {},
@@ -18,27 +17,43 @@ const currentColors = {
   base_back: {},
 }
 
+// genereer random kleur
+
+function getRandomRgb() {
+  return {
+    r: Math.round(Math.random()*205 + 50), // nummer tussen 50 en 255
+    g: Math.round(Math.random()*205 + 50),
+    b: Math.round(Math.random()*205 + 50),
+  }
+}
+
+// random sneaker
+function generateRandomSneaker() {
+
+  currentColors.accent_top = getRandomRgb()
+  currentColors.accent_bottom = getRandomRgb()
+  currentColors.base_front = getRandomRgb()
+  currentColors.base_back = getRandomRgb()
+
+  for (let color of colorAccentTop) {
+    color.style.fill = `rgb(${currentColors.accent_top.r},${currentColors.accent_top.g},${currentColors.accent_top.b})`
+  }
+  for (let color of colorAccentBottom) {
+    color.style.fill = `rgb(${currentColors.accent_bottom.r},${currentColors.accent_bottom.g},${currentColors.accent_bottom.b})`
+  }
+  for (let color of colorBaseFront) {
+    color.style.fill = `rgb(${currentColors.base_front.r},${currentColors.base_front.g},${currentColors.base_front.b})`
+  }
+   for (let color of colorBaseBack) {
+    color.style.fill = `rgb(${currentColors.base_back.r},${currentColors.base_back.g},${currentColors.base_back.b})`
+  }
+}
+
+// uitvoeren tijdens inladen
 generateRandomSneaker()
-predictSneakerCombinations()
-countDataPoints()
 
-stars.forEach((star, i) => {
-  const score = i / 4
-  star.addEventListener("mouseenter", setStars.bind(setStars, i))
-  star.addEventListener("mouseleave", clearStars)
-  star.addEventListener("click", saveTrainingData.bind(saveTrainingData, score))
-})
+// uitvoeren als er op sterretjes wordt geklikt
 
-function flushDataPoints() {
-  window.localStorage.removeItem("trainingData")
-  location.reload()
-}
-flush.addEventListener("click", flushDataPoints)
-
-function countDataPoints() {
-  const data = JSON.parse(window.localStorage.trainingData)
-  data_size.innerHTML = data.length
-}
 function saveTrainingData(score) {
   const data = JSON.parse(window.localStorage.trainingData)
 
@@ -67,25 +82,30 @@ function saveTrainingData(score) {
   countDataPoints()
 }
 
-// once we have a good set of data, generate some color combinations!
+const sneakers = document.getElementById("sneakers")
+
+// Eens we onze dataset hebben, kunnen we suggesties doen
 function predictSneakerCombinations() {
   const data = JSON.parse(window.localStorage.trainingData)
+
   if (data.length < 20) {
     sneakers.innerHTML = "Je hebt minstents 20 beoordeling nodig ..."
     return;
   }
 
   sneakers.innerHTML = ""
-  const net = new brain.NeuralNetwork({activation: "leaky-relu"});
+
+  const net = new brain.NeuralNetwork({activation: "leaky-relu"})
+  net.train(data) // train het neuraal netwerk
+
   const results = []
-
-  net.train(data)
-
   for (let i = 0; i < 100000; i++) {
+
     const accent_top = getRandomRgb()
     const accent_bottom = getRandomRgb()
     const base_front = getRandomRgb()
     const base_back = getRandomRgb()
+
     const colors = [
       Math.round(accent_top.r/2.55) / 100, // divide by 255 and round to 2 decimal places
       Math.round(accent_top.g/2.55) / 100,
@@ -105,20 +125,23 @@ function predictSneakerCombinations() {
     results.push({ accent_top, accent_bottom, base_front, base_back, score})
   }
 
-  // sort results
+  // sorteer de resultaten op basis van hun score
   const sortedResults = results.sort(function(a, b) {
     var a = a.score
     var b = b.score
     return b - a
   })
-  console.log(sortedResults)
-  // keep the top 20 results!
-  for (let i = 0; i < 20; i++) {
 
+  // toon de 20 beste resultaten
+  for (let i = 0; i < 20; i++) {
     addNewSneaker(sortedResults[i])
   }
 }
 
+// uitvoeren tijdens inladen
+predictSneakerCombinations()
+
+// voeg een sneaker toe aan de oplijsting van suggesties
 function addNewSneaker({accent_top, accent_bottom, base_front, base_back, score}) {
   const newSneaker = sneakerWrapper.cloneNode(true)
   newSneaker.classList.add("predicted-sneaker")
@@ -142,6 +165,18 @@ function addNewSneaker({accent_top, accent_bottom, base_front, base_back, score}
   sneakers.appendChild(newSneaker)
 }
 
+
+// sterren configureren
+
+const stars = document.querySelectorAll(".star")
+
+stars.forEach((star, i) => {
+  const score = i / 4
+  star.addEventListener("mouseenter", setStars.bind(setStars, i))
+  star.addEventListener("mouseleave", clearStars)
+  star.addEventListener("click", saveTrainingData.bind(saveTrainingData, score))
+})
+
 function setStars(whichStar) {
   for (let i = 0; i < stars.length; i++) {
     stars[i].classList.add("gold")
@@ -157,48 +192,25 @@ function clearStars() {
   }
 }
 
-function generateRandomSneaker() {
+// data reset
 
-  currentColors.accent_top = getRandomRgb() // getRandomBackgroundRgb()
-  currentColors.accent_bottom = getRandomRgb()
-  currentColors.base_front = getRandomRgb()
-  currentColors.base_back = getRandomRgb()
+const flush = document.getElementById("flush")
 
-  for (let color of colorAccentTop) {
-    console.log('setting colorAccentTop')
-    color.style.fill = `rgb(${currentColors.accent_top.r},${currentColors.accent_top.g},${currentColors.accent_top.b})`
-  }
-  for (let color of colorAccentBottom) {
-    color.style.fill = `rgb(${currentColors.accent_bottom.r},${currentColors.accent_bottom.g},${currentColors.accent_bottom.b})`
-  }
-  for (let color of colorBaseFront) {
-    color.style.fill = `rgb(${currentColors.base_front.r},${currentColors.base_front.g},${currentColors.base_front.b})`
-  }
-   for (let color of colorBaseBack) {
-    color.style.fill = `rgb(${currentColors.base_back.r},${currentColors.base_back.g},${currentColors.base_back.b})`
-  }
+function flushDataPoints() {
+  window.localStorage.removeItem("trainingData")
+  location.reload()
+}
+flush.addEventListener("click", flushDataPoints)
+
+// update data punt teller
+
+const data_size = document.getElementById("data_size")
+
+function countDataPoints() {
+  const data = JSON.parse(window.localStorage.trainingData)
+  data_size.innerHTML = data.length
 }
 
-function getRandomRgb() {
-  return {
-    r: Math.round(Math.random()*205 + 50), // number between 50 and 255
-    g: Math.round(Math.random()*205 + 50),
-    b: Math.round(Math.random()*205 + 50),
-  }
-}
+// uitvoeren bij inladen
 
-function getRandomBackgroundRgb() {
-  return {
-    r: Math.round(Math.random()*50), // number between 0 and 50
-    g: Math.round(Math.random()*50),
-    b: Math.round(Math.random()*50),
-  }
-}
-
-function getRandomSoftRgb() {
-  return {
-    r: Math.round(Math.random()*55 + 200), // number between 200 and 255
-    g: Math.round(Math.random()*55 + 200),
-    b: Math.round(Math.random()*55 + 200),
-  }
-}
+countDataPoints()
